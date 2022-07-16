21be13cd77d6b87f93c905b1dc82a0f1ab2dc4c3
Alex <alexander.ogier@gmail.com>
2022-07-16 21:36:12 +1200

    fix shuffle

--- game.js
+++ game.js
@@ -195,8 +195,26 @@ function Game(code, host) {
 		this.broadcastTiles();
 	}
 
+	function shuffle(array) {
+		let currentIndex = array.length,  randomIndex;
+	  
+		// While there remain elements to shuffle.
+		while (currentIndex != 0) {
+	  
+		  // Pick a remaining element.
+		  randomIndex = Math.floor(Math.random() * currentIndex);
+		  currentIndex--;
+	  
+		  // And swap it with the current element.
+		  [array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+		}
+	  
+		return array;
+	  }
+
 	this.populateFactories = function() {
-		this.bag = this.bag.sort((a, b) => Math.random()-0.5);
+		this.bag = shuffle(this.bag);
 
 		var numberOne = this.lid.filter(t => t.colour == 5)[0];
 		this.lid = this.lid.filter(t => t.colour != 5);
